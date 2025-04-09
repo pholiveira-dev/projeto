1a0cd026ab25a332d81a4a45c8e050cf0835af7a
Pedro Henrique <pedro.alves@escs.edu.br>
2025-04-09 10:20:04 -0300

    Exemplo de template temp

--- recipes/urls.py
+++ recipes/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
-from recipes.views import contato, home, sobre
+from recipes.views import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre),
+    path('', include('recipes.urls')),
 ]
