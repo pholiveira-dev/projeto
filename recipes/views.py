b46e9c611921f12dad0cf462ee33645f2eed655b
Pedro Henrique <pedro.alves@escs.edu.br>
2025-04-10 10:02:58 -0300

    Iniciando o projeto

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name':'Pedro Henrique',})
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'recipes/home.html')
 
 # Create your views here.
