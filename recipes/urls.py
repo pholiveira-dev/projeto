488b27b10c639825742a1dccd559add35368c87e
Pedro Henrique <pedro.alves@escs.edu.br>
2025-04-23 08:43:03 -0300

    Static files

--- recipes/urls.py
+++ recipes/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import path
-from recipes.views import include, path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('recipes.urls')),
+    path('', views.home),
+    path('recipes/<int:id>', views.recipe)
 ]
