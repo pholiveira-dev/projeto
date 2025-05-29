cf809e230df484ea22e32f6d475425b2a969d716
pholiveira-dev <pedro.alves@escs.edu.br>
2025-05-28 22:03:03 -0300

    Primeiro Django Model e seus atributos

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65) # Vai ser o equivalente a um varchar na base de dados
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()# Aqui, todo espaço vai virar hífen, por exemplo: curso-de-django-para-iniciantes
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.CharField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField() # Aqui é uma área livre pro usuário poder digitar
+    preparation_steps_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True) # auto_now_add vai gerar exatamente a data e o horário quando o usuário enviar a receita para o sistema
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+
+
+# EDITED
+# title description slug
+# preparation_time preparation_time_unit
+# servings servings_unit
+# preparation_step
+# preparation_step_is_html
+# created_at updated_at
+# is_published
+# cover
+# category (Relação)
+# Author (Relação)
